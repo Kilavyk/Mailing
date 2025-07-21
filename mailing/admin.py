897bda2a9c4562c5405173a8ad142d287cacb90a
user <kilavyk@yandex.ru>
2025-07-21 22:53:54 +0300

    Creating admin

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from mailing.models import Recipient, Message, Mailing, MailingAttempt
 
-# Register your models here.
+admin.site.register(Recipient)
+admin.site.register(Message)
+admin.site.register(Mailing)
+admin.site.register(MailingAttempt)
