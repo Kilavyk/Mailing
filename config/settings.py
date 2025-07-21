cf02f88347153589a4ad1ce446dfe07d7a7d7f4a
user <kilavyk@yandex.ru>
2025-07-21 22:43:18 +0300

    fix settings.py

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,9 @@ DATABASES = {
         "PASSWORD": os.getenv('PASSWORD'),
         "HOST": os.getenv('HOST'),
         "PORT": os.getenv('PORT'),
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+        }
     }
 }
 
@@ -125,7 +128,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = '#'  # Куда перенаправлять после входа
 LOGOUT_REDIRECT_URL = '#'  # Куда перенаправлять после выхода
