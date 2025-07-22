ec4d17638e0d004e684381e222f00d94d48f5851
user <kilavyk@yandex.ru>
2025-07-22 19:46:28 +0300

    Manually sending emails

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib import messages
+from mailing.services import send_mailing_manual
+from mailing.models import Mailing
 
-# Create your views here.
+
+def start_mailing(request, mailing_id):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    mailing = get_object_or_404(Mailing, id=mailing_id, owner=request.user)
+
+    if send_mailing_manual(mailing_id):
+        messages.success(request, 'Рассылка успешно запущена!')
+    else:
+        messages.error(request, 'Ошибка при отправке рассылки.')
+
+    return redirect('mailing_detail', mailing_id=mailing_id)
