54d0b246c3c7c20facd62f9f97a5c5d61e8675cf
user <kilavyk@yandex.ru>
2025-07-23 21:17:09 +0300

    Statistics for the main page

--- home/views.py
+++ home/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from mailing.models import Mailing  # Для статистики на главной
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'home/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Статистика для главной страницы
+        if self.request.user.is_authenticated:
+            context['user_mailings'] = Mailing.objects.filter(owner=self.request.user).count()
+            context['active_mailings'] = Mailing.objects.filter(
+                owner=self.request.user,
+                status='started'
+            ).count()
+        else:
+            context['user_mailings'] = 0
+            context['active_mailings'] = 0
+
+        return context
+
+
+class ContactsView(TemplateView):
+    template_name = 'home/contacts.html'
