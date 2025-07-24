c85a2597791efea3333a132000c0295941d60f26
user <kilavyk@yandex.ru>
2025-07-24 22:29:41 +0300

    fix path static

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import UserLoginView, UserLogoutView, UserRegisterView, UserProfileView
 
 app_name = 'users'
@@ -8,4 +10,4 @@ urlpatterns = [
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
