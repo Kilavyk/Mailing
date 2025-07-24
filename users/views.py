db396cd1ec58a2104f3eab6b84f25fbdd37b5917
user <kilavyk@yandex.ru>
2025-07-24 22:28:16 +0300

    fix avatar

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import CustomUser
 from .forms import UserRegisterForm, UserProfileForm
+from django.urls import reverse_lazy
 
 
 class UserLoginView(LoginView):
@@ -23,7 +24,14 @@ class UserProfileView(UpdateView):
     model = CustomUser
     form_class = UserProfileForm
     template_name = 'users/profile.html'
+    success_url = reverse_lazy('users:profile')
 
     def get_object(self, queryset=None):
         return self.request.user
-    
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        if 'avatar' in self.request.FILES:
+            self.object.avatar = self.request.FILES['avatar']
+        self.object.save()
+        return super().form_valid(form)
