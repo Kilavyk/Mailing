180b164746bb6723efd38d33fc5a1b745f30e822
user <kilavyk@yandex.ru>
2025-07-25 23:02:49 +0300

    optimizing fields

--- users/forms.py
+++ users/forms.py
@@ -1,36 +1,130 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, UserCreationForm
 from .models import CustomUser
 
 
-class UserRegisterForm(UserCreationForm):
-    class Meta:
-        model = CustomUser
-        fields = ('email', 'password1', 'password2', 'first_name', 'last_name', 'phone_number', 'country')
-
+class BaseFormStyle:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field in self.fields:
-            self.fields[field].widget.attrs.update({'class': 'form-control'})
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
+            if isinstance(field, forms.BooleanField):
+                field.widget.attrs.update({'class': 'form-check-input'})
 
 
-class UserProfileForm(forms.ModelForm):
+class UserRegisterForm(BaseFormStyle, UserCreationForm):
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(attrs={'autocomplete': 'username'})
+    )
+    password1 = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text=""
+    )
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text=""
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'first_name', 'last_name', 'phone_number', 'country', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email'].lower()
+        if commit:
+            user.save()
+        return user
+
+
+class UserLoginForm(BaseFormStyle, AuthenticationForm):
+    username = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(attrs={'autocomplete': 'username'})
+    )
+    password = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'})
+    )
+
+
+class UserProfileForm(BaseFormStyle, UserChangeForm):
+    password = None
+
     class Meta:
         model = CustomUser
         fields = ('first_name', 'last_name', 'phone_number', 'country', 'avatar')
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields:
-            self.fields[field].widget.attrs.update({'class': 'form-control'})
-
     def clean_avatar(self):
         avatar = self.cleaned_data.get('avatar')
         if avatar:
-            # Проверка размера файла (2MB)
             if avatar.size > 2 * 1024 * 1024:
                 raise forms.ValidationError("Файл слишком большой (макс. 2MB)")
-            # Проверка расширения
             if not avatar.name.lower().endswith(('.jpg', '.jpeg', '.png')):
                 raise forms.ValidationError("Неподдерживаемый формат файла")
         return avatar
+
+
+class UserDeleteForm(BaseFormStyle, forms.Form):
+    confirm = forms.BooleanField(
+        label="Я подтверждаю удаление аккаунта",
+        required=True,
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class UserRegisterForm(UserCreationForm):
+#     class Meta:
+#         model = CustomUser
+#         fields = ('email', 'password1', 'password2', 'first_name', 'last_name', 'phone_number', 'country')
+#
+#
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         for field in self.fields:
+#             if self.fields[field].widget.attrs.get('class'):
+#                 self.fields[field].widget.attrs['class'] += ' form-control'
+#             else:
+#                 self.fields[field].widget.attrs.update({'class': 'form-control'})
+#             if field in ['password1', 'password2']:
+#                 self.fields[field].help_text = None
+#
+#
+# class UserProfileForm(forms.ModelForm):
+#     password = None  # Убираем поле смены пароля
+#     class Meta:
+#         model = CustomUser
+#         fields = ('first_name', 'last_name', 'phone_number', 'country', 'avatar')
+#
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         for field in self.fields:
+#             self.fields[field].widget.attrs.update({'class': 'form-control'})
+#
+#     def clean_avatar(self):
+#         avatar = self.cleaned_data.get('avatar')
+#         if avatar:
+#             # Проверка размера файла (2MB)
+#             if avatar.size > 2 * 1024 * 1024:
+#                 raise forms.ValidationError("Файл слишком большой (макс. 2MB)")
+#             # Проверка расширения
+#             if not avatar.name.lower().endswith(('.jpg', '.jpeg', '.png')):
+#                 raise forms.ValidationError("Неподдерживаемый формат файла")
+#         return avatar
--- users/views.py
+++ users/views.py
@@ -1,23 +1,43 @@
 from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import CustomUser
-from .forms import UserRegisterForm, UserProfileForm
+from .forms import UserDeleteForm, UserLoginForm, UserProfileForm, UserRegisterForm
 from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.views.generic import FormView, RedirectView, TemplateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class UserLoginView(LoginView):
     template_name = 'users/login.html'
+    form_class = UserLoginForm
+    authentication_form = UserLoginForm
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('home:home')
+
 
 
 class UserLogoutView(LogoutView):
-    pass
+    url = reverse_lazy('home:home')
+
+
+    def dispatch(self, request, *args, **kwargs):
+        logout(request)
+        messages.info(request, 'Вы успешно вышли из системы')
+        return super().dispatch(request, *args, **kwargs)
 
 
 class UserRegisterView(CreateView):
-    model = CustomUser
-    form_class = UserRegisterForm
     template_name = 'users/register.html'
-    success_url = '/'
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        messages.success(self.request,"Регистрация прошла успешно! Теперь вы можете войти в систему.")
+        return super().form_valid(form)
 
 
 class UserProfileView(UpdateView):
@@ -35,3 +55,102 @@ class UserProfileView(UpdateView):
             self.object.avatar = self.request.FILES['avatar']
         self.object.save()
         return super().form_valid(form)
+
+class DeleteAccountView(LoginRequiredMixin, FormView):
+    template_name = 'users/delete_account.html'
+    form_class = UserDeleteForm
+    success_url = reverse_lazy('catalog:home')
+
+    def form_valid(self, form):
+        user = self.request.user
+        logout(self.request)
+        user.delete()
+        messages.success(self.request, 'Ваш аккаунт был успешно удален')
+        return super().form_valid(form)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# from django.views.generic import CreateView, UpdateView
+# from django.contrib.auth.views import LoginView, LogoutView
+# from .models import CustomUser
+# from .forms import UserRegisterForm, UserProfileForm
+# from django.urls import reverse_lazy
+# from django.contrib import messages
+# from django.contrib.auth import logout
+#
+#
+# class UserLoginView(LoginView):
+#     template_name = 'users/login.html'
+#     redirect_authenticated_user = True
+#     success_url = reverse_lazy('home:home')
+#
+#     def form_valid(self, form):
+#         """Вызывается при успешной авторизации"""
+#         response = super().form_valid(form)
+#         return response
+#
+#     def form_invalid(self, form):
+#         messages.error(
+#             self.request,
+#             "Ошибка входа. Проверьте правильность email и пароля."
+#         )
+#         return super().form_invalid(form)
+#
+#
+# class UserLogoutView(LogoutView):
+#     url = reverse_lazy('home:home')
+#
+#     def dispatch(self, request, *args, **kwargs):
+#         logout(request)
+#         messages.info(request, 'Вы успешно вышли из системы')
+#         return super().dispatch(request, *args, **kwargs)
+#
+#
+# class UserRegisterView(CreateView):
+#     model = CustomUser
+#     form_class = UserRegisterForm
+#     template_name = 'users/register.html'
+#     success_url = reverse_lazy('users:login')
+#
+#     def form_valid(self, form):
+#         response = super().form_valid(form)
+#         messages.success(self.request,"Регистрация прошла успешно! Теперь вы можете войти в систему.")
+#         return response
+#
+#
+# class UserProfileView(UpdateView):
+#     model = CustomUser
+#     form_class = UserProfileForm
+#     template_name = 'users/profile.html'
+#     success_url = reverse_lazy('users:profile')
+#
+#     def get_object(self, queryset=None):
+#         return self.request.user
+#
+#     def form_valid(self, form):
+#         self.object = form.save(commit=False)
+#         if 'avatar' in self.request.FILES:
+#             self.object.avatar = self.request.FILES['avatar']
+#         self.object.save()
+#         return super().form_valid(form)
