5ac8c43b886d24cd41d6f3d43c927cd3d4258f9c
user <kilavyk@yandex.ru>
2025-07-26 18:36:11 +0300

    add moderator

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, ContactsView
+from .views import HomeView, ContactsView, ModerationView
 
 app_name = 'home'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('moderation/', ModerationView.as_view(), name='moderation'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
 from mailing.models import Mailing, Recipient
+from users.models import CustomUser
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib import messages
 
 
 class HomeView(TemplateView):
@@ -35,3 +40,34 @@ class HomeView(TemplateView):
 
 class ContactsView(TemplateView):
     template_name = 'home/contacts.html'
+
+
+class ModerationView(UserPassesTestMixin, TemplateView):
+    template_name = 'home/moderation.html'
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['users'] = CustomUser.objects.all()
+        context['mailings'] = Mailing.objects.all()
+        context['unique_recipients'] = Recipient.objects.values('email').distinct()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if 'block_user' in request.POST:
+            user_id = request.POST.get('block_user')
+            user = CustomUser.objects.get(id=user_id)
+            user.is_active = False
+            user.save()
+            messages.success(request, f'Пользователь {user.email} заблокирован')
+
+        elif 'block_mailing' in request.POST:
+            mailing_id = request.POST.get('block_mailing')
+            mailing = Mailing.objects.get(id=mailing_id)
+            mailing.status = 'completed'
+            mailing.save()
+            messages.success(request, f'Рассылка #{mailing.id} отключена')
+
+        return redirect(reverse('home:moderation'))
