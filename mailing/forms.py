f0f52f7ab769a631aa4efff747a48a76363475a5
user <kilavyk@yandex.ru>
2025-07-26 17:18:32 +0300

    add recipient's and message's form

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from .models import Mailing, Message, Recipient
 
 
@@ -22,3 +24,32 @@ class MailingForm(forms.ModelForm):
 
         for field in self.fields:
             self.fields[field].widget.attrs.update({'class': 'form-control'})
+
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['subject', 'body']
+        widgets = {
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+        }
+
+
+class RecipientForm(forms.ModelForm):
+    class Meta:
+        model = Recipient
+        fields = ['email', 'full_name', 'comment']
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'full_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'comment': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise forms.ValidationError("Введите корректный email адрес")
+        return email
